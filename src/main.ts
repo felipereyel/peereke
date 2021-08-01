8760b8d7122ca9804a3bdf6d6b0a6d166ebcd374
felipe reyel <felipe.reyel@gmail.com>
2021-07-31 21:10:02 -0300

    add peerjs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import { ExpressPeerServer } from 'peer';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/', (_, res) => res.send('OK'));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const peerServer = ExpressPeerServer(server, {
+  key: process.env.SECRET,
+  allow_discovery: true,
+});
+
+app.use('/', peerServer);
